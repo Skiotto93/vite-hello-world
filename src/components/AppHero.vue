cbf7fc01742cfa3041807814452a3f0efa309c72
skiotto93 <fabio.forenza@hotmail.com>
2022-11-23 17:26:28 +0100

    Aggiunto AppHero che figata!!!

---
+++ src/components/AppHero.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      title:'Ha Bootstrap!!!!',
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti rem nihil itaque officiis nostrum quisquam voluptatibus debitis tempora! Rerum, unde illo. Consequuntur odit reiciendis voluptas tenetur minus odio veritatis blanditiis.'
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h2 class="display-4 mt-3">{{ title }}</h2>
+            <p class="lead">{{ text }}</p>
+            
+        </div>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
